1fbb90e56a434c73a6c734bf9e23c4b7d7239188
Olle Rämsberg <olle@ramsberg.nu>
2020-01-20 23:27:41 +0100

    Rewrite to class

--- rovare.py
+++ rovare.py
@@ -1,15 +1,22 @@
 
-konsonanter = "bcdfghjklmnpqrstvwxz"
+class RovarTranslator:
+	def translate(self,text):
+		konsonanter = "bcdfghjklmnpqrstvwxz"
+		self.rovartext = ""
+		for bokstav in text:
+			if bokstav in konsonanter:
+				output = bokstav + "o" + bokstav
+			else:
+				output = bokstav
+			self.rovartext += output
+		return(self.rovartext)
+
+rt = RovarTranslator()
 
 text = input("Text:")
 
-rovartext= ""
+kk = rt.translate(text)
 
-for bokstav in text:
-	if bokstav in konsonanter:
-		output = bokstav+"o"+bokstav
-	else:
-		output = bokstav
-	rovartext += output
+print("Translated:",kk)
 
-print(rovartext)
+
